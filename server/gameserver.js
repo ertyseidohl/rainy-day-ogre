39fce72cf4f12e867f1a716466ace0c2f258e7dd
Erty Seidel <eseidel@snap-interactive.com>
2015-04-05 17:20:32 -0400

    Start building out actions in gameserver

--- server/gameserver.js
+++ server/gameserver.js
@@ -33,4 +33,30 @@ app.get('/startgame', function(req, res){
     });
 });
 
+app.get('/action', function(req,res) {
+    switch (action) {
+        case "move":
+            //cannot move into a square containing an enemy (use ram instead)
+            //only gevs can move after an attack has been made
+            break;
+        case "split":
+            //split an infantry into multiple infantry units
+            break;
+        case "attack":
+            //attack: [source1, source2, ... sourceN], target (part or unit)
+            break;
+        case "endturn":
+            //includes recovery phase
+            break;
+        case "ram":
+            //must ram into a square containing an enemy
+            break;
+
+    }
+});
+
+app.get('/poll', function(req,res) {
+
+});
+
 app.listen(8001);
